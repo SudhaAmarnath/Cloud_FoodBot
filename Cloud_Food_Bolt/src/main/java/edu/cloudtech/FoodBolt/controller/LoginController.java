934a563b2f01e85343ce6a851c2643bc8caf4a3a
Mukesh R Sahay <mukeshranjan.sahay@sjsu.edu>
2018-12-08 20:44:55 -0800

    FoodBolt UI changes

--- Cloud_Food_Bolt/src/main/java/edu/cloudtech/FoodBolt/controller/LoginController.java
+++ Cloud_Food_Bolt/src/main/java/edu/cloudtech/FoodBolt/controller/LoginController.java
@@ -2,7 +2,9 @@ package edu.cloudtech.FoodBolt.controller;
 
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.cloudtech.FoodBolt.dao.CustomerDetails;
 import edu.cloudtech.FoodBolt.dao.ServiceProvider;
 import edu.cloudtech.FoodBolt.dao.TableReservation;
+import edu.cloudtech.FoodBolt.service.CustomerDetailsService;
 import edu.cloudtech.FoodBolt.service.ReservationService;
 import edu.cloudtech.FoodBolt.service.ServiceProvideRowMapper;
 import edu.cloudtech.FoodBolt.service.ServiceProviderService;
@@ -34,6 +41,9 @@ public class LoginController {
 	
 	@Autowired
 	ReservationService reservationService;
+	
+	@Autowired
+	CustomerDetailsService customerDetailService;
 	
 	@GetMapping("/login")
 	public String getLogin() {
@@ -105,6 +115,58 @@ public class LoginController {
 		}
 	
 			
+	}
+	
+	@RequestMapping(value = "/addSessionForGoogle", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public String addSessionForGoogle(@RequestBody CustomerDetails customerDetails, HttpSession session) {
+		
+		String Username = customerDetails.getEmail();
+		String Password = customerDetails.getPassword();
+		String DB_Username = "";
+		String DB_Password = "";
+		String First_Name ="";
+		String Middle_Name ="";
+		String Last_Name ="";
+		String default_no_of_Guests="";
+		String CuisineType="";
+		String SELECT_SQL = "SELECT * FROM CUST_DETAILS where Email = ?";
+		
+		session.setAttribute("isLoggedIn", true);
+		session.setAttribute("email", Username);
+
+		System.out.println("In add Session Controller" + Username);
+
+		CustomerDetails user = null;
+		try {
+			 user = jdbcTemplate.queryForObject(SELECT_SQL, new UserRowMapper(), customerDetails.getEmail());
+		} catch(DataAccessException dae) {
+			dae.printStackTrace();
+		}
+		
+		if(user == null) {
+			customerDetailService.addCustomerDetails(customerDetails);
+			try {
+				 user = jdbcTemplate.queryForObject(SELECT_SQL, new UserRowMapper(), customerDetails.getEmail());
+			} catch(DataAccessException dae) {
+				dae.printStackTrace();
+			}
+		}
+		
+		DB_Username =user.getEmail();
+		DB_Password =user.getPassword();
+		First_Name=user.getFirst_name();
+		Last_Name=user.getLast_name();
+		CuisineType=user.getPref_cuisin_typ();
+
+		default_no_of_Guests=user.getDefault_guests();
+		
+		session.setAttribute("firstName", First_Name);
+		session.setAttribute("middleName", Middle_Name);
+		session.setAttribute("lastName", Last_Name);
+		session.setAttribute("cuisineType", CuisineType);
+		session.setAttribute("default_no_of_guests", default_no_of_Guests);
+		return "success";
 	}
 	
 	@GetMapping("/loginservice")
@@ -188,16 +250,25 @@ public class LoginController {
 
 	
 	@GetMapping("/logout")
-	public String signout(Model model, HttpServletRequest request) {
+	public String signout(Model model, HttpServletRequest request, HttpServletResponse resp) {
 		request.getSession().setAttribute("isLoggedIn", false);
 		request.getSession().setAttribute("firstName", "");
 		request.getSession().setAttribute("middleName", "");
 		request.getSession().setAttribute("lastName", "");
 		request.getSession().setAttribute("cuisineType", "");
 		request.getSession().setAttribute("default_no_of_guests", "");
-	 
 		
-		return "login";
+		Cookie[] cookies = request.getCookies();
+	    if (cookies != null)
+			System.out.println("Clear Cookies..!!");
+	        for (Cookie cookie : cookies) {
+	            cookie.setValue("");
+	            cookie.setPath("/");
+	            cookie.setMaxAge(0);
+	            resp.addCookie(cookie);
+	        }
+		
+		return "index";
 	}
 
 	
